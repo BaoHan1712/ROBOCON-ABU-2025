27513aa51862503e7948277b84c622649164c781
BaoHan1712 <hanquocbao1712@gmail.com>
2025-01-12 16:14:57 +0700

    update code robot

---
+++ robot2_4banh/V_FLAME_MR1/ROBOT_MR1/main.c
@@ -0,0 +1,140 @@
+// #include "cmsis_os.h"
+
+// 19h42 18 03 2023
+
+#include "stm32f4xx.h"
+#include "stm32f4xx_gpio.h"
+#include "stm32f4xx_rcc.h"
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <math.h>
+#include "LID_HMI.h"
+#include "doc_tay_game.h"
+#include "config.h"
+
+#include "4OmniControler.h"
+#include "FreeRTOS.h"
+#include "task.h"
+#include "DieuKhienCoCau.h"
+// #include "ROBOTRUN.h"
+#include "BasicFunction.h"
+#include "San_Xanh.h"
+#include "San_Do.h"
+
+vu8 speed, speed1, update, Home;
+char	shooting = 0;
+
+
+static void taskGyro(void *pvParameters)
+{
+	while (1)
+	{
+		//-------------------------------------------------------------
+
+		//-----------------------------------------------------------------
+
+		HMI_RUN_LOOP(17);
+		vTaskDelay(15);
+	}
+}
+static void taskDieuKhienCoCau(void *pvParameters)
+{
+	while (1)
+	{
+		if(SHARE)	robotGamePadControl();
+		else			robotStop(0);
+		//ADCValue_Control();
+		//Giu_Nong_Ban();
+		
+		vTaskDelay(3);
+	}
+}
+static void taskRobotAnalytics(void *pvParameters)
+{
+	while (1)
+	{
+		
+		robotAnalytics();
+		vTaskDelay(3);
+	}
+}
+static void taskMain(void *pvParameters)
+{
+	Config_out_mode(); // khai b�o ngo ra cua mot chan bat ky
+	Config_in_mode();  // khai bao ngo v�o cua mot chan bat ky
+	// Config_pwm_time_t8();//cai dat timer8 o che do dieu xung
+	//Config_pwm_time_t4(); // cai dat timer4 o che do dieu xung
+	// Config_pwm_time_t9();//cai dat timer9 o che do RC SEVOR
+	Config_encoder_timer2_timer3(); // doc encoder  timer 2, timer 3, timer 5 ,timer 9
+	Config_encoder_timer1();
+	Config_encoder_timer4();
+	// ngat_ngoai();			//chuy�n dung de doc sieu am ket hop timer7, hoac co the dung lam nut nh�n
+	Config_ADC1_DMA(); // su dung  khi doc tin hieu laze hay cac t�n hieu ADC<3,3v
+	// Config_int_time6();		//phuc vu cho chuong trinh hoat dong song song voi he thong v?i chu ky 1ms
+	Config_int_time7();	  // su dung de doc sieu am, tang giam bien, ch�ng nhieu, v� c�c chuong tr�nh hoat dong khong song song voi he thong
+	UART1_DMA_RX(115200); // usart giao tiep voi laban
+	UART2_DMA_TX(115200); /// DIEU KHIEN DONG CO
+	UART3_DMA_RX(115200); // usart giao tiep de doc gamepad
+	// UART4_DMA_RX(115200);	//SU DUNG DE GIAO TIEP MACH DO LAI
+	UART5_DMA_TX(921600); // GIAO TIEP MAN HINH HMI
+	// if (SysTick_Config(SystemCoreClock / 1000))while (1);// 1ms truyen du lieu usart den cac slever
+	UART6_DMA_RX(115200);
+	// reset lai laban
+
+	Home_Mam_xoay(7);
+	robotResetIMU();
+	RESET_ENCODER();
+
+	//---- reset he thong ve vi tri ban dau
+	Vi_tri = 0;
+	update = 1;
+	
+	//taget_BT_Nang_Bong = BT_Bat_Bong_giua;
+	//-----------------------------------
+	xTaskCreate(taskRobotAnalytics, (signed char *)"taskRobotAnalytics", 256, NULL, 0, NULL);
+	xTaskCreate(taskDieuKhienCoCau, (signed char *)"taskDieuKhienCoCau", 256, NULL, 0, NULL);
+
+	vt_xl_nang_lua = 4;
+	xl_nang_lua = 1 ;
+	
+	taget_BT_Xoay = 600;
+	//robotStop(10);
+	while (1)
+	{			
+		while(SHARE) 
+		{
+
+			co_cau_phong_thu();
+
+//			Nang_Ha_Nong_Ban();
+//			tim_ro();
+//			Lay_bong();
+//			xoaytam(received_offset);
+//			Day_re();
+//			
+//			tinhLucBan(lazeTraiValue);
+//			calculate_force(force);
+//			
+//			nhabong();
+//  	  if(!R1 && TRIANGLE)	Re_Bong();
+//			if(!R1 && SQUARE)	dua_bong();
+//			//if(!R1 && R2&&CB_NHAN_BONG_BO_BAN==0)ban_bong_giua();
+//			if(!R1 && LJOY) tra_bong();
+//			
+//			bantheokhoangcach();
+//			//if(CB_NHAN_BONG_BO_BAN == 0)	test_gocban();
+			vTaskDelay(1);
+		}
+		robotStop(0);
+	}
+}
+
+
+int main(void)
+{
+	xTaskCreate(taskMain, (signed char *)"taskMain", 256, NULL, 0, NULL);
+	xTaskCreate(taskGyro, (signed char *)"taskGyro", 256, NULL, 0, NULL);
+
+	vTaskStartScheduler(); // lenh nay cho phep cac tac vu da nhiem hoat dong.
+}
