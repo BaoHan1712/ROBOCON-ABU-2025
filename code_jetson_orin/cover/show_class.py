3bfa7fc445e875da8e0d2fa341414b97dd43d6b1
BaoHan1712 <hanquocbao1712@gmail.com>
2025-03-13 11:08:05 +0700

    update v3

--- code_jetson_orin/cover/show_class.py
+++ code_jetson_orin/cover/show_class.py
@@ -1,7 +1,7 @@
 from ultralytics import YOLO
 
 # Load a pretrained YOLO11n model
-model = YOLO("yolo11s.pt")
+model = YOLO(r"model\2clss_yolov8_ver1.pt")
 
 print(model.names) 
 # model.export(format="onnx", dynamic=True, simplify=True)
